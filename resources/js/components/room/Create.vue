f4cb6af14efac9059f88a8440fd598e1824e0cde
J <j@j.local>
2022-05-10 11:32:22 +0200

    Escape Room

--- resources/js/components/room/Create.vue
+++ resources/js/components/room/Create.vue
@@ -1,13 +1,86 @@
 <template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header"><h4>Create Room</h4></div>
+                <div class="card-body">
+                    <form @submit.prevent="create">
+                        <div class="row">
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" v-model="room.name">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Date</label>
+                                    <input type="date" class="form-control" v-model="room.date">
+                                </div>
+                            </div>
 
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Select the game:</label>
+                                    <select @change="getGameValue($event)" name="room-select" class="form-select">
+                                        <option selected>-</option>
+                                        <option v-for="g in games" :key="g.id" :value="g.id">{{ g.name }}</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+
 export default {
-    name: "Create"
+    name: "createRoom",
+    data() {
+        return {
+            room: {
+                name: "asdfasfdf",
+                date: "sdfsdf",
+                available: "1",
+                game_id: ""
+            },
+            games: []
+        }
+    },
+    mounted() {
+        this.readGames()
+    },
+    methods: {
+        async create() {
+            await this.axios.post('/api/room', this.review).then(response => {
+                this.$router.push({name: "showRoom"})
+            }).then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log(error.response)
+            });
+
+        },
+        async readGames() {
+            await this.axios.get('/api/game').then(response => {
+                this.games = response.data
+            }).catch(error => {
+                console.log(error)
+                this.games = []
+            })
+        },
+
+        getGameValue(event) {
+            this.room.game_id = event.target.value;
+        }
+    }
 }
 </script>
-
-<style scoped>
-
-</style>
--- resources/js/components/user/Create.vue
+++ resources/js/components/user/Create.vue
@@ -1,13 +1,85 @@
 <template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header"><h4>Create User</h4></div>
+                <div class="card-body">
+                    <form @submit.prevent="create">
+                        <div class="row">
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" v-model="user.name">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>DNI</label>
+                                    <input type="text" minlength="9" maxlength="9" class="form-control"
+                                           v-model="user.dni">
+                                </div>
+                            </div>
 
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Phone</label>
+                                    <input type="text" minlength="9" maxlength="9" class="form-control"
+                                           v-model="user.phone">
+                                </div>
+                            </div>
+
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" v-model="user.email">
+                                </div>
+                            </div>
+
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" minlength="5" maxlength="20" class="form-control"
+                                           v-model="user.password">
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+
 export default {
-    name: "Create"
+    name: "createUser",
+    data() {
+        return {
+            user: {
+                name: "",
+                dni: "",
+                phone: "",
+                email: "",
+                password: ""
+            },
+        }
+    },
+    methods: {
+        async create() {
+            await this.axios.post('/api/user', this.review).then(response => {
+                this.$router.push({name: "showUser"})
+            }).then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log(error.response)
+            });
+
+        },
+    }
 }
 </script>
-
-<style scoped>
-
-</style>
